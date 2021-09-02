7e750b8863338ae2087eb26611807c97168cc826
Diony <dionybusker@gmail.com>
2021-09-02 12:59:45 +0200

    OOP exercise

---
+++ Avenger.php
@@ -0,0 +1,8 @@
+<?php
+
+    class Avenger extends Superhero {
+        public function __construct($name, $gender, $oneliner) {
+            $team = 'Avengers';
+            parent::__construct($name, $gender, $team, $oneliner);
+        }
+    }
---
+++ Superhero.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Superhero {
+        public $name;
+        public $gender;
+        public $team;
+        public $oneliner;
+
+        public function __construct($name, $gender, $team, $oneliner) {
+            $this->name = $name;
+            $this->gender = $gender;
+            $this->team = $team;
+            $this->oneliner = $oneliner;
+        }
+
+        public function __toString() {
+            return json_encode($this);
+        }
+
+        public function sayOneliner() {
+            echo '<h2>' . $this->oneliner . '</h2>';
+        }
+    }
